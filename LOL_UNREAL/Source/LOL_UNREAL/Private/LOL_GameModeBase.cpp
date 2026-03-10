9bafd02925953a93a300de4f422ef59e76def04e
DESKTOP-QCIEGQR\임성훈 <cmw0210@naver.com>
2026-03-10 21:12:58 +0900

    BaseChampion 상속 받은 알리스타 추가

--- LOL_UNREAL/Source/LOL_UNREAL/Private/LOL_GameModeBase.cpp
+++ LOL_UNREAL/Source/LOL_UNREAL/Private/LOL_GameModeBase.cpp
@@ -1,17 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "LOL_GameModeBase.h"
 #include "LOL_PlayerController.h"
-#include "LOL_Character.h"
+#include "Champion_Alistar.h"
 
 ALOL_GameModeBase::ALOL_GameModeBase()
 {
     PlayerControllerClass = ALOL_PlayerController::StaticClass();
-
-    static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/vain/bp_lolcharacter"));
-    if (PlayerPawnBPClass.Class != NULL)
-    {
-        DefaultPawnClass = PlayerPawnBPClass.Class;
-    }
+    DefaultPawnClass = AChampion_Alistar::StaticClass();
 }
 
 void ALOL_GameModeBase::BeginPlay()
