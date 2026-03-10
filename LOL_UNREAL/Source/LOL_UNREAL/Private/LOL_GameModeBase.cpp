4dbbc6fbe77c342e118ac2ae5df8b27e97af6707
DESKTOP-QCIEGQR\임성훈 <cmw0210@naver.com>
2026-03-10 19:30:50 +0900

    컨트롤러와 캐릭터 연결

--- LOL_UNREAL/Source/LOL_UNREAL/Private/LOL_GameModeBase.cpp
+++ LOL_UNREAL/Source/LOL_UNREAL/Private/LOL_GameModeBase.cpp
@@ -1,8 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "LOL_GameModeBase.h"
+#include "LOL_PlayerController.h"
+#include "LOL_Character.h"
 
 ALOL_GameModeBase::ALOL_GameModeBase()
 {
+    PlayerControllerClass = ALOL_PlayerController::StaticClass();
+
+    static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/vain/bp_lolcharacter"));
+    if (PlayerPawnBPClass.Class != NULL)
+    {
+        DefaultPawnClass = PlayerPawnBPClass.Class;
+    }
 }
 
 void ALOL_GameModeBase::BeginPlay()
