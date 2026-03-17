1de5bd05a81b482e7cac3e01d7a7c5dd77a37e5b
parkjihun216 <vlslrtm586@naver.com>
2026-03-17 17:22:13 +0900

    채력바 생성

---
+++ LOL_UNREAL/Source/LOL_UNREAL/Private/LOL_ChampionHpBarWidget.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LOL_ChampionHpBarWidget.h"
+#include "Components/ProgressBar.h"
+
+ULOL_ChampionHpBarWidget::ULOL_ChampionHpBarWidget(const FObjectInitializer& ObjectInitializer) :Super(ObjectInitializer)
+{
+	MaxHp = -1.0f;
+}
+
+void ULOL_ChampionHpBarWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	HpProgressBar = Cast<UProgressBar>(GetWidgetFromName(TEXT("PbHpBar")));
+	ensure(HpProgressBar);
+}
+
+void ULOL_ChampionHpBarWidget::UpdateHpBar(float NewCurrentHp)
+{
+	ensure(MaxHp > 0.0f);
+	if (HpProgressBar)
+	{
+		HpProgressBar->SetPercent(NewCurrentHp / MaxHp);
+	}
+}
