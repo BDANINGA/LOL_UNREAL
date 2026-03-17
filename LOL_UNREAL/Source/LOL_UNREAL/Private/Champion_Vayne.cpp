ddaf815d92af2455bd64e8ab52cdb1574bed2938
DESKTOP-QCIEGQR\임성훈 <cmw0210@naver.com>
2026-03-17 17:19:12 +0900

    애니메이션블루프린트 연결, AttackMontage연결

--- LOL_UNREAL/Source/LOL_UNREAL/Private/Champion_Vayne.cpp
+++ LOL_UNREAL/Source/LOL_UNREAL/Private/Champion_Vayne.cpp
@@ -14,9 +14,23 @@ AChampion_Vayne::AChampion_Vayne()
 	{
 		GetMesh()->SetSkeletalMesh(VayneMesh.Object);
 
-		// 2. 캐릭터 캡슐 안에 메쉬 위치 및 회전 정렬
+		// 캐릭터 캡슐 안에 메쉬 위치 및 회전 정렬
 		// 보통 아래로 90유닛, 앞(X축)을 바라보게 -90도 회전 시킵니다.
 		GetMesh()->SetRelativeLocation(FVector(0.f, 0.f, -90.f));
 		GetMesh()->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+
+		// 애니메이션 블루프린트 연결
+		static ConstructorHelpers::FClassFinder<UAnimInstance> VayneABP(TEXT("/Script/Engine.AnimBlueprint'/Game/Level/vain_real/abp_unreal_vain.abp_unreal_vain_C'"));
+		if (VayneABP.Succeeded())
+		{
+			GetMesh()->SetAnimInstanceClass(VayneABP.Class);
+			GetMesh()->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+		}
+
+		static ConstructorHelpers::FObjectFinder<UAnimMontage> VayneAttackMtg(TEXT("/Script/Engine.AnimMontage'/Game/Level/vain_real/am_attack_1.am_attack_1'"));
+		if (VayneAttackMtg.Succeeded())
+		{
+			AttackMontage = VayneAttackMtg.Object;
+		}
 	}
 }
