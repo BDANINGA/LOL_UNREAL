6d097315df8046ed3ca97da4581775e3289afd58
DESKTOP-QCIEGQR\임성훈 <cmw0210@naver.com>
2026-03-24 16:05:10 +0900

    Alistar 애니메이션 연결 및 자잘한 오타 수정

--- LOL_UNREAL/Source/LOL_UNREAL/Private/Champion_Alistar.cpp
+++ LOL_UNREAL/Source/LOL_UNREAL/Private/Champion_Alistar.cpp
@@ -8,15 +8,27 @@
 AChampion_Alistar::AChampion_Alistar()
 {
 	// 스켈레탈 메쉬 에셋 연결
-	static ConstructorHelpers::FObjectFinder<USkeletalMesh> AlistarMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Level/alistar/unrea_alistar_real_1.unrea_alistar_real_1'"));
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> AlistarMesh(TEXT("/Game/Level/alistar/unrea_alistar_real_1.unrea_alistar_real_1"));
 
 	if (AlistarMesh.Succeeded())
 	{
 		GetMesh()->SetSkeletalMesh(AlistarMesh.Object);
 
-		// 2. 캐릭터 캡슐 안에 메쉬 위치 및 회전 정렬
-		// 보통 아래로 90유닛, 앞(X축)을 바라보게 -90도 회전 시킵니다.
 		GetMesh()->SetRelativeLocation(FVector(0.f, 0.f, -90.f));
 		GetMesh()->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+
+		// 애니메이션 블루프린트 연결
+		static ConstructorHelpers::FClassFinder<UAnimInstance> AlistarABP(TEXT("/Game/Level/alistar/abp_alistar.abp_alistar_C"));
+		if (AlistarABP.Succeeded())
+		{
+			GetMesh()->SetAnimInstanceClass(AlistarABP.Class);
+			GetMesh()->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+		}
+
+		/*static ConstructorHelpers::FObjectFinder<UAnimMontage> AlistarAttackMtg(TEXT("/Script/Engine.AnimMontage'/Game/Level/vain_real/am_attack_1.am_attack_1'"));
+		if (AlistarAttackMtg.Succeeded())
+		{
+			AttackMontage = AlistarAttackMtg.Object;
+		}*/
 	}
 }
